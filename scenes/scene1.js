c295a1c65572d3c12a00fc19304e5210bd68a66b
GabrielHMA01 <159061439+GabrielHMA01@users.noreply.github.com>
2024-03-02 20:15:25 -0300

    adiciona sprite da moeda

--- scenes/scene1.js
+++ scenes/scene1.js
@@ -6,18 +6,27 @@ class Scene1 extends Phaser.Scene {
     }
     preload() {
       this.load.image("background1", "assets/m_merged.png")
-      this.load.image("inteliLogo", "assets/.png")
-      this.load.image("botaoJogar", "assets/.png")
+      this.load.spritesheet("coin", "assets/coin.png", { frameWidth: 200, frameHeight: 300 })
   
     }
-  
+
     create() {
       // Carrega a cena Main Menu
       this.mainMenu = this.add.image(gameDimensions.width/2, gameDimensions.height/2, "background1").setScale(1.4)
-      this.logoInteli = this.add.image(100, 200, "")
-      this.botaoJogar = this.add.image(900, 575, "")
+      this.coin = this.add.sprite(100.6, 500, "coin").setScale(0.25)
+      this.anims.create({
+        key: "animar",
+        frames: this.anims.generateFrameNumbers("coin", { start: 0, end: 5 }),
+        frameRate: 6,
+        repeat: -1
+      })
+      this.coin.anims.play("animar", true)
+
+      // this.load.image("", "assets/.png")
+      // this.botaoJogar = this.add.image(900, 575, "")
 
     }
+    
   
     update() {}
     
