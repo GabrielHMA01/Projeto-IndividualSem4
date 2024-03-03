0882669494ae74a2d38f40c8d69175cc157f0e22
GabrielHMA01 <159061439+GabrielHMA01@users.noreply.github.com>
2024-03-02 21:36:39 -0300

    Co-authored-by: LeoAndriolo <LeoAndriolo@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,28 +1,25 @@
-
 // Cria as configurações para Phaser.Game
+var fundo1 = new Image()
+fundo1.src = "assets/m_merged.png"
+
 const gameDimensions = {
-    width: 1280,
-    height: 580,
+  width: 1280,
+  height: 580,
 }
 const config = {
-    type: Phaser.AUTO, // Ajusta o renderizador automaticamente (WebGL e Canvas)
-    width: gameDimensions.width,  // Define a largura para 1280 pixels
-    height: gameDimensions.height, // Define a altura
-    physics: {
-        default: 'arcade',
-        arcade: {
-            gravity: { y: 400 },
-            debug: true
-        }
+  type: Phaser.AUTO, // Ajusta o renderizador automaticamente (WebGL e Canvas)
+  width: gameDimensions.width, // Define a largura para 1280 pixels
+  height: gameDimensions.height, // Define a altura
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 400 },
+      debug: true,
     },
+  },
 
-    scene: [
-        Menu, Scene1
-    ],
-    
-};
+  scene: [Menu, Scene1],
+}
 
 // Cria o jogo colocando a variável config como atributo(parâmetro)
-const game = new Phaser.Game(config);
-
-
+const game = new Phaser.Game(config)
